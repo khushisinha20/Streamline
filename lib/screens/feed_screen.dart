a98ff10c4f496b7a6b06288d850bb11b13fa6887
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2023-05-08 15:56:18 +0530

    Added Bottom NavBar UI

---
+++ lib/screens/feed_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FeedScreen extends StatefulWidget {
+  const FeedScreen({super.key});
+
+  @override
+  State<FeedScreen> createState() => FeedScreenState();
+}
+
+class FeedScreenState extends State<FeedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
