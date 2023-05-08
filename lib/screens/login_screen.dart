59c1554a78a82019d22795e75b42bff0d03e063a
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2023-05-08 18:21:50 +0530

    Fixed Memory Leaks and Added Loaders

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:streamline/resources/auth_methods.dart';
 import 'package:streamline/screens/home_screen.dart';
 import 'package:streamline/widgets/custom_button.dart';
 import 'package:streamline/widgets/custom_textfield.dart';
+import 'package:streamline/widgets/loading_indicator.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String routeName = '/login';
@@ -17,18 +18,35 @@ class LoginScreenState extends State<LoginScreen> {
   final TextEditingController _passwordController = TextEditingController();
   final AuthMethods _authMethods = AuthMethods();
 
+  bool _isLoading = false;
+
   loginUser() async {
+    setState(() {
+      _isLoading = true;
+    });
+
     bool res = await _authMethods.loginUser(
       context,
       _emailController.text,
       _passwordController.text,
     );
+
+    setState(() {
+      _isLoading = false;
+    });
+
     if (res) {
       Navigator.pushReplacementNamed(context, HomeScreen.routeName);
     }
   }
 
   @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
@@ -36,42 +54,44 @@ class LoginScreenState extends State<LoginScreen> {
       appBar: AppBar(
         title: const Text('Log In'),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 18.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: size.height * 0.1,
+      body: _isLoading
+          ? const LoadingIndicator()
+          : SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: size.height * 0.1,
+                    ),
+                    const Text(
+                      'Email',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: CustomTextField(controller: _emailController),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      'Password',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: CustomTextField(controller: _passwordController),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    CustomButton(onTap: loginUser, text: 'Log In'),
+                  ],
+                ),
               ),
-              const Text(
-                'Email',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: CustomTextField(controller: _emailController),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              const Text(
-                'Password',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: CustomTextField(controller: _passwordController),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              CustomButton(onTap: loginUser, text: 'Log In'),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -3,6 +3,7 @@ import 'package:streamline/screens/home_screen.dart';
 import 'package:streamline/widgets/custom_button.dart';
 import 'package:streamline/widgets/custom_textfield.dart';
 import 'package:streamline/resources/auth_methods.dart';
+import 'package:streamline/widgets/loading_indicator.dart';
 
 class SignupScreen extends StatefulWidget {
   static const String routeName = '/signup';
@@ -17,8 +18,12 @@ class SignupScreenState extends State<SignupScreen> {
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _usernameController = TextEditingController();
   final AuthMethods _authMethods = AuthMethods();
+  bool _isLoading = false;
 
   void signUpUser() async {
+    setState(() {
+      _isLoading = true;
+    });
     bool res = await _authMethods.signUpUser(
       context,
       _emailController.text,
@@ -26,12 +31,23 @@ class SignupScreenState extends State<SignupScreen> {
       _passwordController.text,
     );
 
+    setState(() {
+      _isLoading = false;
+    });
+
     if (res) {
       Navigator.pushReplacementNamed(context, HomeScreen.routeName);
     }
   }
 
   @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _usernameController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
@@ -39,53 +55,55 @@ class SignupScreenState extends State<SignupScreen> {
       appBar: AppBar(
         title: const Text('Sign Up'),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 18.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: size.height * 0.1,
+      body: _isLoading
+          ? LoadingIndicator()
+          : SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: size.height * 0.1,
+                    ),
+                    const Text(
+                      'Email',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: CustomTextField(controller: _emailController),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      'Username',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: CustomTextField(controller: _usernameController),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      'Password',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: CustomTextField(controller: _passwordController),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    CustomButton(onTap: signUpUser, text: 'Sign Up'),
+                  ],
+                ),
               ),
-              const Text(
-                'Email',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: CustomTextField(controller: _emailController),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              const Text(
-                'Username',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: CustomTextField(controller: _usernameController),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              const Text(
-                'Password',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: CustomTextField(controller: _passwordController),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              CustomButton(onTap: signUpUser, text: 'Sign Up'),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
