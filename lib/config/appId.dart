7ee14d6bb2faa3551a2c647deca079e7838e3151
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2023-05-09 00:09:34 +0530

    Agora App ID setup

---
+++ lib/config/appId.dart
@@ -0,0 +1,3 @@
+String appId = "c1ad8deec5bb49ee93200b92d47be16a";
+String tempToken =
+    "007eJxTYPCV+11z70d/bg37+m4L8WxNqRe3986a+it8097m4lSH+Z4KDMmGiSkWKampyaZJSSaWqamWxkYGBkmWRikm5kmphmaJrZaRKQ2BjAyGe68wMTJAIIjPzlCSWlxiaGTMwAAAEfAhYA==";
--- lib/screens/broadcast_screen.dart
+++ lib/screens/broadcast_screen.dart
@@ -1,4 +1,6 @@
+import 'package:agora_rtc_engine/rtc_engine.dart';
 import 'package:flutter/material.dart';
+import 'package:streamline/config/appId.dart';
 
 class BroadcastScreen extends StatefulWidget {
   const BroadcastScreen({super.key});
@@ -8,7 +10,47 @@ class BroadcastScreen extends StatefulWidget {
 }
 
 class _BroadcastScreenState extends State<BroadcastScreen> {
+  late final RtcEngine _engine;
+  List<int> remoteUid = [];
+
   @override
+  void initState() {
+    super.initState();
+    _initEngine();
+  }
+
+  void _initEngine() async {
+    _engine = await RtcEngine.createWithContext(RtcEngineContext(appId));
+    await _engine.initialize(RtcEngineContext(appId));
+    _addListeners();
+  }
+
+  void _addListeners() {
+    _engine.setEventHandler(RtcEngineEventHandler(
+      joinChannelSuccess: (channel, uid, elapsed) {
+        debugPrint('joinChannelSuccess $channel $uid $elapsed');
+      },
+      userJoined: (uid, elapsed) {
+        debugPrint('userJoined $uid $elapsed');
+        setState(() {
+          remoteUid.add(uid);
+        });
+      },
+      userOffline: (uid, reason) {
+        debugPrint('userOffline $uid $reason');
+        setState(() {
+          remoteUid.removeWhere((element) => element == uid);
+        });
+      },
+      leaveChannel: (stats) {
+        debugPrint('Leave Channel $stats');
+        setState(() {
+          remoteUid.clear();
+        });
+      },
+    ));
+  }
+
   Widget build(BuildContext context) {
     return Container(
       child: Text('Hello'),
