cea96127eb25d61af75a2224aeadebe24fee2849
Khushi Sinha <khushisinha@AJITs-MacBook-Pro.local>
2023-05-08 18:12:41 +0530

    Added Picking Image from Gallery Featue

--- lib/screens/go_live_screen.dart
+++ lib/screens/go_live_screen.dart
@@ -1,6 +1,8 @@
 import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:streamline/utils/colors.dart';
+import 'package:streamline/utils/utils.dart';
 import 'package:streamline/widgets/custom_button.dart';
 import 'package:streamline/widgets/custom_textfield.dart';
 
@@ -12,6 +14,7 @@ class GoLiveScreen extends StatefulWidget {
 
 class _GoLiveScreenState extends State<GoLiveScreen> {
   final TextEditingController _titleController = TextEditingController();
+  Uint8List? image;
 
   @override
   void dispose() {
@@ -28,42 +31,62 @@ class _GoLiveScreenState extends State<GoLiveScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 22.0, vertical: 20.0),
-                child: DottedBorder(
-                  borderType: BorderType.RRect,
-                  radius: const Radius.circular(10),
-                  dashPattern: const [10, 4],
-                  strokeCap: StrokeCap.round,
-                  color: buttonColor,
-                  child: Container(
-                    width: double.infinity,
-                    height: 150,
-                    decoration: BoxDecoration(
-                      color: buttonColor.withOpacity(0.05),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Icon(
-                          Icons.folder_open,
-                          color: buttonColor,
-                          size: 40,
-                        ),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        Text(
-                          'Select your thumbnail',
-                          style:
-                              TextStyle(fontSize: 15, color: Colors.grey[400]),
-                        ),
-                      ],
+              Column(
+                children: [
+                  GestureDetector(
+                    onTap: () async {
+                      Uint8List? pickedImage = await pickImage();
+                      if (pickedImage != null) {
+                        setState(() {
+                          image = pickedImage;
+                        });
+                      }
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 22.0, vertical: 20.0),
+                      child: image != null
+                          ? SizedBox(
+                              height: 300,
+                              child: Image.memory(image!),
+                            )
+                          : DottedBorder(
+                              borderType: BorderType.RRect,
+                              radius: const Radius.circular(10),
+                              dashPattern: const [10, 4],
+                              strokeCap: StrokeCap.round,
+                              color: buttonColor,
+                              child: Container(
+                                width: double.infinity,
+                                height: 150,
+                                decoration: BoxDecoration(
+                                  color: buttonColor.withOpacity(0.05),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const Icon(
+                                      Icons.folder_open,
+                                      color: buttonColor,
+                                      size: 40,
+                                    ),
+                                    const SizedBox(
+                                      height: 15,
+                                    ),
+                                    Text(
+                                      'Select your thumbnail',
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          color: Colors.grey[400]),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
                     ),
                   ),
-                ),
+                ],
               ),
               const SizedBox(
                 height: 15,
